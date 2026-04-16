55233cc5eb1c3d04658ce3091b6e67ddacf8efaa
Tim <t.lenssen@student.fontys.nl>
2026-04-16 11:34:49 +0200

    Replace while loop with for loop and experiment with alternating bar colors

--- Moving Banners Prorotype 1/sketch.js
+++ Moving Banners Prorotype 1/sketch.js
@@ -1,12 +1,23 @@
-function setup() { 
-    createCanvas(400, 700); 
-} 
+function setup() {
+  createCanvas(400, 700);
+}
 
-function draw() { 
-    background(200); 
-   
-    let y = 0; 
-    while( y < height) { 
-        rect(0, y, width, 50); 
-        y +=50;} 
+function draw() {
+  background(200);
+  let bannerNumber = 0;
+
+for(let y = 0; y < height; y+=50) {
+ 
+  
+if (bannerNumber % 2 === 0) {
+  fill(255, 0, 0);
+}
+ else {
+  fill(0, 0, 255);
+}
+  rect(0, y, width, 50);
+  
+  bannerNumber++;
+}
+  
 }
